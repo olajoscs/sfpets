90ef0133af49cba6a58dd7dbf3c4453a6d8578ca
Csaba Olajos <olajos.csaba.88@gmail.com>
2020-06-17 20:26:57 +0200

    Add ShadowPetSeeder to DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(PetCategorySeeder::class);
         $this->call(LocationSeeder::class);
+        $this->call(ShadowPetSeeder::class);
     }
 }
