416688e4a389b4995d8bb65b2f53fa6797605346
Csaba Olajos <olajos.csaba.88@gmail.com>
2020-07-28 19:35:38 +0200

    Show toast message when action is done

--- resources/js/components/Pet.vue
+++ resources/js/components/Pet.vue
@@ -1,5 +1,5 @@
 <template>
-    <li class="collection-item avatar">
+    <li class="collection-item avatar" :class="{fade: shouldFade}">
         <img src="https://picsum.photos/50" alt="" class="circle responsive-img">
         <span class="title"><strong>{{ pet.name }}</strong></span>
         <p>
@@ -7,22 +7,22 @@
             {{ pet.description }}
         </p>
         <div class="secondary-content">
-            <a v-if="pet.isFound" @click="markAsFound(false)"
+            <a v-if="pet.isFound" @click="markAsFound(false, `Mégsincs megszerezve: ${pet.name}`)"
                     class="btn-small red lighten-3"
                     title="Mégsincs megszerezve">
                 <i class="material-icons">check</i>
             </a>
-            <a v-else-if="pet.isDiscovered" @click="markAsFound(true)"
+            <a v-else-if="pet.isDiscovered" @click="markAsFound(true, `Megszereztem: ${pet.name}`)"
                     class="btn-small"
                     title="Megszereztem">
                 <i class="material-icons">check</i>
             </a>
-            <a v-if="pet.isDiscovered" @click="markAsDiscovered(false)"
+            <a v-if="pet.isDiscovered" @click="markAsDiscovered(false, `Mégsincs felfedezve: ${pet.name}`)"
                     class="btn-small red lighten-3"
                     title="Mégsincs felfedezve">
                 <i class="material-icons">search</i>
             </a>
-            <a v-else @click="markAsDiscovered(true)"
+            <a v-else @click="markAsDiscovered(true, `Felfedeztem: ${pet.name}`)"
                     class="btn-small"
                     title="Felfedeztem">
                 <i class="material-icons">search</i>
@@ -33,6 +33,7 @@
 
 <script>
     import {mapActions, mapGetters} from 'vuex';
+    import helpers from '../helpers';
 
     export default {
         name: "Pet",
@@ -49,17 +50,23 @@
             ...mapGetters(['getPet']),
             pet() {
                 return this.getPet(this.id);
+            },
+
+            shouldFade() {
+                return this.pet.isDiscovered;
             }
         },
 
         methods: {
             ...mapActions(['setFound', 'setDiscovered']),
 
-            markAsFound: function(isFound) {
+            markAsFound: function(isFound, message) {
+                helpers.toast(message);
                 this.setFound({petId: this.pet.id, isFound});
             },
 
-            markAsDiscovered: function(isDiscovered) {
+            markAsDiscovered: function(isDiscovered, message) {
+                helpers.toast(message);
                 this.setDiscovered({petId: this.pet.id, isDiscovered});
             },
         }
--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -2,6 +2,10 @@ class Helpers {
     initUiComponents() {
         M.AutoInit();
     }
+
+    toast(message) {
+        M.toast({html: message})
+    }
 }
 
 export default new Helpers();
