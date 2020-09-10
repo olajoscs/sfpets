8902184ca9c0bc87980c6eacea068f1d655745ab
csaba.olajos <olajos.csaba.88@gmail.com>
2020-09-10 11:29:06 +0200

    Rewrite menu structure

--- resources/js/components/NavBar/NavBar.vue
+++ resources/js/components/NavBar/NavBar.vue
@@ -1,29 +1,40 @@
 <template>
     <div>
-        <nav>
+        <ul id="dropdown-locale" class="dropdown-content">
+            <li><a href="/locale/set/hu"><img src="/images/hungary.svg" :alt="$t('text.language_hungarian')"></a></li>
+            <li><a href="/locale/set/en"><img src="/images/uk.svg" :alt="$t('text.language_english')"></a></li>
+        </ul>
+        <nav class="nav">
             <div class="nav-wrapper">
-                <a href="#" class="brand-logo center">{{ $t(`text.app_title`) }}</a>
-                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                <ul class="right hide-on-med-and-down center">
-                    <menu-list :classes="[]" id="''"></menu-list>
+                <a href="#" class="brand-logo left">{{ $t(`text.app_title`) }}</a>
+                <ul class="right">
+                    <li>
+                        <a href="#">
+                            <i class="material-icons left">account_box</i>
+                            <div class="truncate player-name">Árvíztűrő tükörfúrógép</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-button dropdown-trigger"
+                           href="#!"
+                           data-activates="dropdown-locale"
+                           data-target="dropdown-locale">
+                            <i class="material-icons left locale-icon">flag</i>
+                            <i class="material-icons right locale-dropdown-icon">arrow_drop_down</i>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </nav>
-
-        <menu-list :classes="['sidenav']"
-                   :id="'mobile-demo'"></menu-list>
     </div>
-
 </template>
 
 <script>
-    import MenuList from './MenuList';
-
     export default {
         name: "Localization",
 
         components: {
-            MenuList
+
         },
 
         data: () => {
@@ -33,5 +44,24 @@
 </script>
 
 <style scoped>
+    .locale-icon {
+        margin-right: 0;
+        padding-right: 15px;
+    }
 
+    .locale-dropdown-icon {
+        margin-left: 0;
+    }
+
+    .player-name {
+        width: 50px;
+    }
+
+    #dropdown-locale a {
+        padding: 5px 5px 0 5px;
+    }
+
+    #dropdown-locale a img {
+        width: 40px;
+    }
 </style>
