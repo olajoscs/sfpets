1e393bf59a4b57d38011916e90e3424bd6259de7
csaba.olajos <olajos.csaba.88@gmail.com>
2020-11-11 12:56:00 +0100

    Favicon

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -5,6 +5,39 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ __('pets.app_title') }}</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="shortcut Icon"
+              href="/images/pets/fire-egg.png" type="image/x-icon"/>
+        <link rel="apple-touch-icon" sizes="57x57"
+              href="/images/pets/fire-egg.png">
+        <link rel="apple-touch-icon" sizes="60x60"
+              href="/images/pets/fire-egg.png">
+        <link rel="apple-touch-icon" sizes="72x72"
+              href="/images/pets/fire-egg.png">
+        <link rel="apple-touch-icon" sizes="76x76"
+              href="/images/pets/fire-egg.png">
+        <link rel="apple-touch-icon" sizes="114x114"
+              href="/images/pets/fire-egg.png">
+        <link rel="apple-touch-icon" sizes="120x120"
+              href="/images/pets/fire-egg.png">
+        <link rel="apple-touch-icon" sizes="144x144"
+              href="/images/pets/fire-egg.png">
+        <link rel="apple-touch-icon" sizes="152x152"
+              href="/images/pets/fire-egg.png">
+        <link rel="apple-touch-icon" sizes="180x180"
+              href="/images/pets/fire-egg.png">
+        <link rel="icon" type="image/png" sizes="192x192"
+              href="/images/pets/fire-egg.png">
+        <link rel="icon" type="image/png" sizes="32x32"
+              href="/images/pets/fire-egg.png">
+        <link rel="icon" type="image/png" sizes="96x96"
+              href="/images/pets/fire-egg.png">
+        <link rel="icon" type="image/png" sizes="16x16"
+              href="/images/pets/fire-egg.png">
+        <meta name="msapplication-TileImage"
+              content="/images/pets/fire-egg.png">
+        <meta name="msapplication-TileColor"
+              content="#ffffff">
+        <meta name="theme-color" content="#ffffff">
     </head>
     <body>
         <div id="app"></div>
